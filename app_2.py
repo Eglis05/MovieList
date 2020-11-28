f3c5013162dddd18b16715558a0ae6a210e3fa47
Eglis Balani <eglisbalani5@gmail.com>
2020-11-28 19:12:10 +0100

    Changed GUI

---
+++ app_2.py
@@ -0,0 +1,212 @@
+import sys, os
+from datetime import datetime
+from functools import partial
+import json
+import time
+
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QIcon, QIntValidator
+from PyQt5.QtWidgets import QDesktopWidget
+
+from movielist import MovieList #probably turn it in a class importing
+
+FILE_SAVE_LOCATION = "/home/eglis/Desktop/repos/MovieList"
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()  # Call the inherited classes init method
+        uic.loadUi('lister.ui', self)  # Load the .ui file
+        self.setWindowTitle("Eglis Listing")
+        self.center()
+        self.setWindowIcon(QIcon('movielist.png'))
+
+        # dropdown selector
+        self.inputType = self.findChild(QtWidgets.QComboBox, 'inputSelect')
+        self.inputType.activated[str].connect(self.inputTypeSelector)
+
+        # input field
+        self.inputPath = self.findChild(QtWidgets.QLineEdit, 'TxtPath')
+
+        # output field
+        self.outputPath = self.findChild(QtWidgets.QLineEdit, 'ListPath')
+
+        # name add field
+        self.nameadd = self.findChild(QtWidgets.QLineEdit, 'NameAdd')
+
+        # browse button for file
+        self.btnBrowsein = self.findChild(QtWidgets.QPushButton, 'btnBrowseIn')
+        self.btnBrowsein.clicked.connect(partial(self.fileSelector, "in"))
+        self.btnBrowsein.hide()
+        self.btnBrowseout = self.findChild(QtWidgets.QPushButton, 'btnBrowseout')
+        self.btnBrowseout.clicked.connect(partial(self.fileSelector, "out"))
+        self.btnBrowseout.hide()
+
+        # start stop button
+        self.isRunning = False
+        self.startBtn = self.findChild(QtWidgets.QPushButton, 'btnStart')
+        self.startBtn.clicked.connect(self.startBtnPress)
+
+        # scale button
+        self.scaleBtn = self.findChild(QtWidgets.QPushButton, 'btnScale')
+        self.scaleBtn.clicked.connect(self.scaleBtnPress)
+
+        # top list button
+        self.topBtn = self.findChild(QtWidgets.QPushButton, 'btnTop')
+        self.topBtn.clicked.connect(self.topBtnPress)
+
+        # remove button
+        self.removeBtn = self.findChild(QtWidgets.QPushButton, 'btnRemove')
+        self.removeBtn.clicked.connect(self.removeBtnPress)
+
+        # add button
+        self.addBtn = self.findChild(QtWidgets.QPushButton, 'btnAdd')
+        self.addBtn.clicked.connect(self.addBtnPress)
+
+        # error msg
+        self.errorLbl = self.findChild(QtWidgets.QLabel, 'error_msg')
+        self.errorLbl.hide()
+
+        # the config id reading part
+        self.configId = self.findChild(QtWidgets.QLineEdit, 'inptConfigId')
+        self.lblConf = self.findChild(QtWidgets.QLabel, 'lblConfig')
+        self.inputConf = self.findChild(QtWidgets.QComboBox, 'selectConf')
+        self.getConfigs()
+        self.inputConf.activated[str].connect(self.selectConfig)
+        self.savedConfigFile = ""
+
+        # the nr of points to add should only be int
+        self.onlyInt = QIntValidator()
+        self.points = self.findChild(QtWidgets.QLineEdit, 'Points')
+        self.points.setValidator(self.onlyInt)
+
+        #calling the function for creating the first instance
+        self.inputTypeSelector(self.inputType.currentText())
+
+        self.show()  # Show the GUI
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def enableDisableUIEle(self, enable):
+        self.inputType.setEnabled(enable)
+        self.inputConf.setEnabled(enable)
+        self.inputPath.setEnabled(enable)
+        self.outputPath.setEnabled(enable)
+        self.configId.setEnabled(enable)
+
+    def scaleBtnPress(self):
+        self.algorithms.scale()
+
+    def topBtnPress(self):
+        print("Top Button is Work in Progress!")
+
+    def removeBtnPress(self):
+        self.algorithms.remove(self.nameadd.text())
+    
+    def addBtnPress(self):
+        self.algorithms.add(self.nameadd.text(), self.points.text())
+
+    def processtest(self):
+        print("Processing is Work in Progress!")
+
+    def startBtnPress(self):
+        if not self.isRunning:
+            self.enableDisableUIEle(False)
+            self.isRunning = True
+            self.startBtn.setText("Stop")
+            print("Processing starting from file: {}".format(self.inputPath.text()))
+            start_time = time.time()
+            self.processtest()
+            print('... done. it took {} seconds'.format(time.time()-start_time))
+            self.enableDisableUIEle(True)
+            self.isRunning = False
+            self.startBtn.setText("Start")
+            self.errorLbl.setText("Finish processing the file")
+            self.errorLbl.setStyleSheet('color: green')
+            self.errorLbl.show()
+
+        else:
+            self.enableDisableUIEle(True)
+            self.isRunning = False
+            self.startBtn.setText("Start")
+
+    def getConfigs(self):
+        saved_config = []
+        self.inputConf.clear()
+        if str(self.inputType.currentText()) == "Movies":
+            saved_config = os.listdir("Movies")
+        elif str(self.inputType.currentText()) == "Books":
+            saved_config = os.listdir("Books")
+        
+        if len(saved_config) > 0:
+            self.inputConf.addItem("")
+            for conf in saved_config:
+                if not conf.startswith('.'):
+                    self.inputConf.addItem(conf.replace(".json", ""))
+            self.inputConf.show()
+            self.lblConf.show()
+        else:
+            self.inputConf.hide()
+            self.lblConf.hide()
+
+    def selectConfig(self, value):
+        if value == "":
+            self.savedConfigFile = value
+            self.configId.setText(value)
+            self.inputPath.setText(value)
+            self.outputPath.setText(value)
+        else:
+            self.configId.setText(value)
+            self.generateConfigFile()
+            with open(self.savedConfigFile) as json_file:
+                config_data = json.load(json_file)
+                self.inputPath.setText(config_data["input_path"])
+                self.outputPath.setText(config_data["output_path"])
+
+    def generateConfigFile(self):
+        if self.configId.text() == "":
+            now = datetime.now()
+            self.configId.setText(now.strftime("%d%m%Y_%H%M%S"))
+        if str(self.inputType.currentText()) == "Movies":
+            self.savedConfigFile = "Movies/" + self.configId.text() + ".json"
+        elif str(self.inputType.currentText()) == "Books":
+            self.savedConfigFile = "Books/" + self.configId.text() + ".json"
+
+    def saveAreasMarked(self):
+        self.generateConfigFile()
+        config_data = {"input_path": self.inputPath.text(), "output_path": self.outputPath.text()}
+        with open(self.savedConfigFile, 'w') as outfile:
+            json.dump(config_data, outfile)
+        if self.configId.text() not in [self.inputConf.itemText(i) for i in range(self.inputConf.count())]:
+            self.getConfigs()
+
+    def inputTypeSelector(self, value):
+        if value == "Movies":
+            self.algorithms = MovieList()
+        elif value == "Books":
+            print("Books is Work in Progress")
+
+        self.inputPath.setText("")
+        self.outputPath.setText("")
+        self.configId.setText("")
+        self.savedConfigFile = ""
+        self.errorLbl.hide()
+        self.getConfigs()
+
+    def fileSelector(self, type):
+        if type == "in":
+            win_name = 'Select a txt file'
+            fileName = QtWidgets.QFileDialog.getOpenFileName(self, win_name, FILE_SAVE_LOCATION)
+            self.inputPath.setText(fileName[0])
+        elif type == "out":
+            win_name = 'Select a txt file'
+            fileName = QtWidgets.QFileDialog.getOpenFileName(self, win_name, FILE_SAVE_LOCATION)
+            self.outputPath.setText(fileName[0])
+
+
+app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
+window = Ui()  # Create an instance of our class
+app.exec_()  # Start the application
--- movielist.py
+++ movielist.py
@@ -1,216 +1,220 @@
 from urllib.request import urlopen
 import requests
-INF = 1000000
+INF = 10000000
 movielist = "movielist.txt"
 
-def addnewmovie(movie, addition):
-    f = open(movielist, "a")
-    f.write(movie + " " + str(addition) + "\n")
-    f.close()
-    updatelist(movielist)
+class MovieList():
+    def __init__(self):
+        print("Creating a MovieList Instance")
 
-def updatelines(lines, wantedfile):
-    f = open(wantedfile, "w")
-    for line in lines:
-        f.write(line + "\n")
-    f.close()
-    if wantedfile == movielist:
-        updatelist(wantedfile)
+    def addnewmovie(self, movie, addition):
+        f = open(movielist, "a")
+        f.write(movie + " " + str(addition) + "\n")
+        f.close()
+        self.updatelist(movielist)
 
-def updatelist(wantedfile):
-    f = open(wantedfile, "r")
-    lines = f.readlines()
-    before = INF
-    OK = 0
-    for i in range(len(lines)):
-        lines[i] = lines[i].split()
-        if before < int(lines[i][1]): 
-            lines[i] = lines[i][0] + " " + lines[i][1]
-            lines[i-1], lines[i] = lines[i], lines[i-1]
-            OK = 1
-        else:
-            before = int(lines[i][1])
-            lines[i] = lines[i][0] + " " + lines[i][1]
-    f.close()
-    if OK:
-        updatelines(lines, wantedfile)
+    def updatelines(self, lines, wantedfile):
+        f = open(wantedfile, "w")
+        for line in lines:
+            f.write(line + "\n")
+        f.close()
+        if wantedfile == movielist:
+            self.updatelist(wantedfile)
 
-def addmovie(movie, addition = 1):
-    f = open(movielist, "r")
-    lines = f.readlines()
-    OK = 1
-    for i in range(len(lines)):
-        lines[i] = lines[i].split()
-        if lines[i][0] != movie:
-            lines[i] = " ".join(lines[i])
-            continue
-        lines[i] = lines[i][0] + " " + str(int(lines[i][1])+addition)
+    def updatelist(self, wantedfile):
+        f = open(wantedfile, "r")
+        lines = f.readlines()
+        before = INF
         OK = 0
-    f.close()
-    if OK:
-        addnewmovie(movie, addition)
-    else:
-        updatelines(lines, movielist)
-
-def add(movie, addition = 1):
-    movie = movie.split()
-    for i in range(len(movie)):
-        movie[i] = movie[i].lower().capitalize()
-    movie2 = " ".join(movie)
-    movie = "+".join(movie)
-    webp = requests.get("https://www.imdb.com/find?q=" + movie).text
-
-    startlist = "<table class=\"findList\">"
-    lenlist = webp.find(startlist, 80000, len(webp)-1) + len(startlist)
-    if lenlist == len(startlist) - 1:
-        print("SOMETHING IS WRONG")
-        return
-    secondone = "<a href=\"/title/"
-    lensecond = len(secondone)
-
-    for i in range(lenlist,len(webp)):
-        if webp[i:(i+lensecond)] == secondone:
-            lensecond += i
-            break
-    endsymbol = '/'
-    endnumber = lensecond
-    for i in range(lensecond,len(webp)):
-        if webp[i] == endsymbol:
-            endnumber = i + 1
-            break
-    title = webp[lensecond:endnumber]
-    webp = requests.get("https://www.imdb.com/title/" + title).text
-
-    
-    lastone = "<meta property='og:title' content=\""
-    lenlastone = len(lastone)
-
-    for i in range(len(webp)):
-        if webp[i:(i+lenlastone)] == lastone:
-            lenlastone += i
-            break
-
-    endsymbol = '('
-    endsecond = lenlastone
-    for i in range(lenlastone,len(webp)):
-        if webp[i] == endsymbol:
-            endsecond = i-1
-            break
-    
-    quotes = "&quot;"
-
-    x = webp.find(quotes, lenlastone, endsecond)
-    if (x != -1):
-        lenlastone = webp.find(quotes, x + len(quotes), endsecond) + len(quotes) + 1
-
-    movie = webp[lenlastone:endsecond]
-    if movie.lower() != movie2.lower():
-        print("Not the same: " + movie2 + " vs " + movie)
-    # else:
-    #     print("The same: " + movie)
-
-    movie = movie.split()
-    movie = "_".join(movie)
-    addmovie(movie, addition)
-
-def readnotes(notes):
-    f = open(notes, "r")
-    lines = f.readlines()
-    for i in range(len(lines)):
-        lines[i] = lines[i].split()
-        movie = lines[i][0:len(lines[i])-1]
-        movie = " ".join(movie)
-        try:
-            add(movie, int(lines[i][len(lines[i])-1]))
-        except ValueError:
-            add(" ".join(lines[i]), 1)
-    f.close()
-
-def writenotes(notes):
-    f = open(movielist, "r")
-    lines = f.readlines()
-    f.close()
-    lines = topmovies(len(lines), 0)
-    updatelines(lines, notes)
-
-def erase():
-    f = open(movielist, "w")
-    f.close()
-
-def scale():
-    f = open(movielist, "r")
-    lines = f.readlines()
-    for i in range(len(lines)):
-        lines[i] = lines[i].split()
-        addmovie(lines[i][0], len(lines)-i-int(lines[i][1]))
-    f.close()
-        
-def notline(lines, j):
-    f = open(movielist, "w")
-    for i in range(len(lines)):
-        if i != j:
-            f.write(lines[i] + "\n")
-    f.close()
-
-def removemovie(movie):
-    f = open(movielist, "r")
-    lines = f.readlines()
-    j = -1
-    for i in range(len(lines)):
-        lines[i] = lines[i].split()
-        if lines[i][0] != movie:
-            if j != -1:
-                lines[i][1] = str(int(lines[i][1])+1)
-            lines[i] = " ".join(lines[i])
-            continue
-        lines[i] = " ".join(lines[i])
-        j = i
-    if j != -1:
-        notline(lines, j)
-    f.close()
-
-def remove(movie):
-    movie = movie.split()
-    for i in range(len(movie)):
-        movie[i] = movie[i].lower().capitalize()
-    movie = "_".join(movie)
-    removemovie(movie)
-
-def topmovies(x, ok = 1):
-    f = open(movielist, "r")
-    lines = f.readlines()
-    movies = []
-    for i in range(min(len(lines),x)):
-        lines[i] = lines[i].split()
-        lines[i][0] = lines[i][0].split('_')
-        lines[i][0] = " ".join(lines[i][0])
-        if ok:
-            lines[i] = " ".join(lines[i])
-            movies.append(lines[i])
-        else:
-            movies.append(lines[i][0])
-    f.close()
-    return movies
-
-def searchmovie(movie):
-    f = open(movielist, "r")
-    lines = f.readlines()
-    movies = []
-    for i in range(len(lines)):
-        lines[i] = lines[i].split()
-        OK = 0
-        for j in range(len(lines[i][0])-len(movie)+1):
-            if lines[i][0][j:len(movie)+j].lower() == movie:
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+            if before < int(lines[i][1]): 
+                lines[i] = lines[i][0] + " " + lines[i][1]
+                lines[i-1], lines[i] = lines[i], lines[i-1]
                 OK = 1
-        lines[i] = " ".join(lines[i])
+            else:
+                before = int(lines[i][1])
+                lines[i] = lines[i][0] + " " + lines[i][1]
+        f.close()
         if OK:
-            movies.append(lines[i])
-    f.close()
-    return movies
+            self.updatelines(lines, wantedfile)
 
-def search(movie):
-    movie = movie.split()
-    for i in range(len(movie)):
-        movie[i] = movie[i].lower()
-    movie = "_".join(movie)
-    return searchmovie(movie)
+    def addmovie(self, movie, addition = 1):
+        f = open(movielist, "r")
+        lines = f.readlines()
+        OK = 1
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+            if lines[i][0] != movie:
+                lines[i] = " ".join(lines[i])
+                continue
+            lines[i] = lines[i][0] + " " + str(int(lines[i][1])+addition)
+            OK = 0
+        f.close()
+        if OK:
+            self.addnewmovie(movie, addition)
+        else:
+            self.updatelines(lines, movielist)
+
+    def add(self, movie, addition = 1):
+        movie = movie.split()
+        for i in range(len(movie)):
+            movie[i] = movie[i].lower().capitalize()
+        movie2 = " ".join(movie)
+        movie = "+".join(movie)
+        webp = requests.get("https://www.imdb.com/find?q=" + movie).text
+
+        startlist = "<table class=\"findList\">"
+        lenlist = webp.find(startlist, 80000, len(webp)-1) + len(startlist)
+        if lenlist == len(startlist) - 1:
+            print("SOMETHING IS WRONG")
+            return
+        secondone = "<a href=\"/title/"
+        lensecond = len(secondone)
+
+        for i in range(lenlist,len(webp)):
+            if webp[i:(i+lensecond)] == secondone:
+                lensecond += i
+                break
+        endsymbol = '/'
+        endnumber = lensecond
+        for i in range(lensecond,len(webp)):
+            if webp[i] == endsymbol:
+                endnumber = i + 1
+                break
+        title = webp[lensecond:endnumber]
+        webp = requests.get("https://www.imdb.com/title/" + title).text
+
+        
+        lastone = "<meta property='og:title' content=\""
+        lenlastone = len(lastone)
+
+        for i in range(len(webp)):
+            if webp[i:(i+lenlastone)] == lastone:
+                lenlastone += i
+                break
+
+        endsymbol = '('
+        endsecond = lenlastone
+        for i in range(lenlastone,len(webp)):
+            if webp[i] == endsymbol:
+                endsecond = i-1
+                break
+        
+        quotes = "&quot;"
+
+        x = webp.find(quotes, lenlastone, endsecond)
+        if (x != -1):
+            lenlastone = webp.find(quotes, x + len(quotes), endsecond) + len(quotes) + 1
+
+        movie = webp[lenlastone:endsecond]
+        if movie.lower() != movie2.lower():
+            print("Not the same: " + movie2 + " vs " + movie)
+        # else:
+        #     print("The same: " + movie)
+
+        movie = movie.split()
+        movie = "_".join(movie)
+        self.addmovie(movie, addition)
+
+    def readnotes(self, notes):
+        f = open(notes, "r")
+        lines = f.readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+            movie = lines[i][0:len(lines[i])-1]
+            movie = " ".join(movie)
+            try:
+                self.add(movie, int(lines[i][len(lines[i])-1]))
+            except ValueError:
+                self.add(" ".join(lines[i]), 1)
+        f.close()
+
+    def writenotes(self, notes):
+        f = open(movielist, "r")
+        lines = f.readlines()
+        f.close()
+        lines = self.topmovies(len(lines), 0)
+        self.updatelines(lines, notes)
+
+    def erase(self):
+        f = open(movielist, "w")
+        f.close()
+
+    def scale(self):
+        f = open(movielist, "r")
+        lines = f.readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+            self.addmovie(lines[i][0], len(lines)-i-int(lines[i][1]))
+        f.close()
+            
+    def notline(self, lines, j):
+        f = open(movielist, "w")
+        for i in range(len(lines)):
+            if i != j:
+                f.write(lines[i] + "\n")
+        f.close()
+
+    def removemovie(self, movie):
+        f = open(movielist, "r")
+        lines = f.readlines()
+        j = -1
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+            if lines[i][0] != movie:
+                if j != -1:
+                    lines[i][1] = str(int(lines[i][1])+1)
+                lines[i] = " ".join(lines[i])
+                continue
+            lines[i] = " ".join(lines[i])
+            j = i
+        if j != -1:
+            self.notline(lines, j)
+        f.close()
+
+    def remove(self, movie):
+        movie = movie.split()
+        for i in range(len(movie)):
+            movie[i] = movie[i].lower().capitalize()
+        movie = "_".join(movie)
+        self.removemovie(movie)
+
+    def topmovies(self, x, ok = 1):
+        f = open(movielist, "r")
+        lines = f.readlines()
+        movies = []
+        for i in range(min(len(lines),x)):
+            lines[i] = lines[i].split()
+            lines[i][0] = lines[i][0].split('_')
+            lines[i][0] = " ".join(lines[i][0])
+            if ok:
+                lines[i] = " ".join(lines[i])
+                movies.append(lines[i])
+            else:
+                movies.append(lines[i][0])
+        f.close()
+        return movies
+
+    def searchmovie(self, movie):
+        f = open(movielist, "r")
+        lines = f.readlines()
+        movies = []
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+            OK = 0
+            for j in range(len(lines[i][0])-len(movie)+1):
+                if lines[i][0][j:len(movie)+j].lower() == movie:
+                    OK = 1
+            lines[i] = " ".join(lines[i])
+            if OK:
+                movies.append(lines[i])
+        f.close()
+        return movies
+
+    def search(self, movie):
+        movie = movie.split()
+        for i in range(len(movie)):
+            movie[i] = movie[i].lower()
+        movie = "_".join(movie)
+        return self.searchmovie(movie)
